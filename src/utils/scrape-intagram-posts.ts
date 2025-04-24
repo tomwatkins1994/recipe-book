c901e2e8fcedf01b8053eab4d74943f61cb4f7f4
tomwatkins1994 <twwatkins@btinternet.com>
2025-04-25 00:37:44 +0100

    Get a unique dataset to avoid cross-contamination

--- src/utils/scrape-intagram-posts.ts
+++ src/utils/scrape-intagram-posts.ts
@@ -1,19 +1,25 @@
-import { PlaywrightCrawler } from "crawlee";
+import { PlaywrightCrawler, Dataset } from "crawlee";
+import { v4 as uuidv4 } from 'uuid';
 
 export async function scrapeInstagramPosts(urls: string[]) {
+  const datasetName = `insta-${uuidv4()}`; 
+  const dataset = await Dataset.open(datasetName);
+
   const crawler = new PlaywrightCrawler({
-    async requestHandler({ request, page, pushData }) {
+    async requestHandler({ request, page }) {
       const title = await page.title();
       const caption = await page.locator("meta[property='og:description']").evaluate((meta) => meta.getAttribute("content"));
       const image = await page.locator("meta[property='og:image']").evaluate((meta) => meta.getAttribute("content"));
 
-      await pushData({ title, url: request.loadedUrl, caption, image });
+      await dataset.pushData({ title, url: request.loadedUrl, caption, image });
     },
     maxRequestsPerCrawl: 1,
   });
 
   await crawler.run(urls);
 
-  const data = await crawler.getData();
+  const data = await dataset.getData();
+  dataset.drop();
+
   return data
 }
