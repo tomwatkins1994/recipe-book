435133d6d6d974c44efcee76bba7ec8833c7dc6d
tomwatkins1994 <twwatkins@btinternet.com>
2025-04-25 00:14:09 +0100

    Changed to be a function

---
+++ src/utils/scrape-intagram-posts.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, it } from 'vitest';
+import { scrapeInstagramPosts } from './scrape-intagram-posts';
+
+describe('scrapeInstagramPosts', () => {
+    it('should scrape Instagram posts', async () => {
+        const urls = ["https://www.instagram.com/p/DI1ngIlg6gz/"]
+        const results = await scrapeInstagramPosts(urls);
+    
+        expect(results.count).toBe(1);
+        expect(results.items[0]).toHaveProperty('title');
+        expect(results.items[0].title.length).toBeGreaterThan(0);
+        expect(results.items[0]).toHaveProperty('url');
+        expect(results.items[0].url).toBe(urls[0]);
+        expect(results.items[0]).toHaveProperty('caption');
+        expect(results.items[0].caption.length).toBeGreaterThan(0);
+        expect(results.items[0]).toHaveProperty('image');
+        expect(results.items[0].image.length).toBeGreaterThan(0);
+    });
+})
--- src/utils/scrape-intagram-posts.ts
+++ src/utils/scrape-intagram-posts.ts
@@ -1,17 +1,19 @@
 import { PlaywrightCrawler } from "crawlee";
 
-const crawler = new PlaywrightCrawler({
-  async requestHandler({ request, page, pushData }) {
-    const title = await page.title();
-    const caption = await page.locator("meta[property='og:description']").evaluate((meta) => meta.getAttribute("content"));
-    const image = await page.locator("meta[property='og:image']").evaluate((meta) => meta.getAttribute("content"));
+export async function scrapeInstagramPosts(urls: string[]) {
+  const crawler = new PlaywrightCrawler({
+    async requestHandler({ request, page, pushData }) {
+      const title = await page.title();
+      const caption = await page.locator("meta[property='og:description']").evaluate((meta) => meta.getAttribute("content"));
+      const image = await page.locator("meta[property='og:image']").evaluate((meta) => meta.getAttribute("content"));
 
-    await pushData({ title, url: request.loadedUrl, caption, image });
-  },
-  maxRequestsPerCrawl: 1,
-});
+      await pushData({ title, url: request.loadedUrl, caption, image });
+    },
+    maxRequestsPerCrawl: 1,
+  });
 
-await crawler.run(["https://www.instagram.com/p/DI1ngIlg6gz/"]);
+  await crawler.run(urls);
 
-const data = await crawler.getData();
-console.table(data.items[0].caption);
+  const data = await crawler.getData();
+  return data
+}
